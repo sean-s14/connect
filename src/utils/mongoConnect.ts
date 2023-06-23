d5aa53ec3e4a7dc07ca56164098ae22d5f1b5c89
Sean Stocker <sean.stocker14@gmail.com>
2023-06-23 18:56:47 +0100

    feat: Integrate axios, mongoose, and custom utility package
    This commit includes the following changes:
    - Installed 'axios' for API requests
    - Installed 'mongoose' for MongoDB interactions
    - Integrated custom utility package '@sean14/utils'
    - Created 'mongoConnect' for MongoDB connection handling
    - Defined global mongoose variable in 'global.d.ts' for universal API usage

---
+++ src/utils/mongoConnect.ts
@@ -0,0 +1,39 @@
+import mongoose, { ConnectOptions } from "mongoose";
+
+const URI = process.env.MONGO_URI;
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export default async function mongoConnect() {
+  if (!URI) {
+    throw new Error(
+      "Please define the URI environment variable inside .env.local"
+    );
+  }
+
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(URI, {
+        bufferCommands: false, // useCreateIndex:true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      } as ConnectOptions)
+      .then((mongoose) => {
+        console.log("Connected to MongoDB");
+        return mongoose;
+      })
+      .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+      });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
